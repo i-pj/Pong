dc71755fda0733fa0d87b56867f2976ab9e2880a
Parth Jain <42parth.jain@gmail.com>
2024-06-26 21:33:26 +0530

    Basic Structure

---
+++ game.py
@@ -0,0 +1,41 @@
+import sys
+import random
+import pygame
+
+pygame.init()
+
+# Global variables
+SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+BALL_RADIUS = 10
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 80
+HALF_PADDLE_WIDTH, HALF_PADDLE_HEIGHT = PADDLE_WIDTH // 2, PADDLE_HEIGHT // 2
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+class Ball:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def update(self):
+        pass
+
+class Paddle:
+    def __init__(self, side):
+        pass
+
+    def update(self):
+        pass
+
+    def handle_input(paddle_left, paddle_right):
+        pass
+
+def main():
+    pass # collision
+
+
+
